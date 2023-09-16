b37105fa6fb13b167bc77314a76dc2efeaa0dc9c
Daredevil-ii <amrrashad603@gmail.com>
2023-09-16 12:51:31 -0700

    New Commit

--- main.h
+++ main.h
@@ -57,7 +57,7 @@ int (*fn)(va_list, char[], int, int, int, int);
 
 
 /**
- * typedef struct fmt fmt_t - Struct op
+ * typedef - Struct op
  *
  * @fmt: The format.
  * @fm_t: The function associated.
